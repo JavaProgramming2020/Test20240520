b4ee4edf6cdf9fa74ebfdece13506fd30ab135f4
YG <JavaProgramming2020@gmail.com>
2024-05-24 14:43:16 +0900

    Commit 5/24.

--- TestG/src/T1.java
+++ TestG/src/T1.java
@@ -1,7 +1,7 @@
 // aha.2
 public class T1 {
 	public static void main(String[] args) {
-		// MAIN.ㅋㅋ
+		// MAIN.ㅋㅋ 5/24
 		String str = "\n...";
 		System.out.print(str);
 		// 이걸 main branch에 merge.
